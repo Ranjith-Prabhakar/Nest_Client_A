6b3dffbf127693e882674238438eb17c7f4abe4f
Ranjith <ranjithp2294@gmail.com>
2025-07-12 23:48:41 +0530

    constant values updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { CLIENT_A_SERVICE_RABBITMQ } from './constants';
+import { CLIENT_A_SERVICE_RABBITMQ, RABBITMQ_URI } from './constants';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { SocketGateway } from './socket.gateway';
 import { RabbitQueueInitializer } from './rabbit-queue.initializer';
@@ -11,7 +11,7 @@ import { RabbitQueueInitializer } from './rabbit-queue.initializer';
         name: CLIENT_A_SERVICE_RABBITMQ,
         transport: Transport.RMQ,
         options: {
-          urls: ['amqp://guest:guest@localhost:5672?heartbeat=60'],
+          urls: [`${RABBITMQ_URI}?heartbeat=60`],
           queue: 'to-clientB',
           queueOptions: {
             durable: true,
